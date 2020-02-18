7ce1184e46ee59a415218fbeb677149b4595c3c2
Jack Rayner <jack.rayner1997@gmail.com>
2020-02-18 21:40:10 +0000

    Added Travis CI push-ability

--- get-keys.sh
+++ get-keys.sh
@@ -5,7 +5,7 @@
 if [[ -z $1 ]] ; then
     echo "No username provided"
 elif which jq >/dev/null 2>&1; then
-    curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key'
+    curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key' > keys
 else
     echo "This script requires JQ to be installed"
 fi
