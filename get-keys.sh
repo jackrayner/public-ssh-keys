1cc70e04e4bc9ea768a78d254dfb095f6e6a42bb
Jack Rayner <jack.rayner1997@gmail.com>
2020-02-18 21:13:50 +0000

    Moved to a script

---
+++ get-keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get GitHub User's public SSH keys
+
+if [[ -z $1 ]] ; then
+    echo "No username provided"
+elif which jq >/dev/null 2>&1; then
+    curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key'
+else
+    echo "This script requires JQ to be installed"
+fi
