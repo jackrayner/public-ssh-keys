9e2bd099f998479d014d900854e6f643c8fdfc7f
Jack Rayner <jack.rayner1997@gmail.com>
2020-02-18 23:08:58 +0000

    Removed auto push

--- get-keys.sh
+++ get-keys.sh
@@ -5,7 +5,7 @@
 if [[ -z $1 ]] ; then
     echo "No username provided"
 elif which jq >/dev/null 2>&1; then
-    curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key' > keys
+    curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key'
 else
     echo "This script requires JQ to be installed"
 fi
