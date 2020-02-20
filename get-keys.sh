1bf042370c38db49b4beca37fd7b9134ea90ddd3
Jack Rayner <jack.rayner1997@gmail.com>
2020-02-20 21:07:22 +0000

    Added license

--- get-keys.sh
+++ get-keys.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Get GitHub User's public SSH keys
+# MIT License
+# Copyright (c) 2020 Jack Rayner <me@jrayner.net>
+
 
 if [[ -z $1 ]] ; then
+## TODO: Add validation check using GitHub API for username.
     echo "No username provided"
 elif which jq >/dev/null 2>&1; then
     curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key'
