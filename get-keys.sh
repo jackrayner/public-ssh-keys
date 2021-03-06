866b29a177d606d529e55260dfd12b4739264a0f
Jack Rayner <jack.rayner1997@gmail.com>
2021-03-06 01:01:57 +0000

    Updated to remove jq

--- get-keys.sh
+++ get-keys.sh
@@ -7,8 +7,11 @@
 if [[ -z $1 ]] ; then
 ## TODO: Add validation check using GitHub API for username.
     echo "No username provided"
-elif which jq >/dev/null 2>&1; then
-    curl -s "https://api.github.com/users/${1}/keys" | jq -r '.[].key'
+elif which curl >/dev/null 2>&1; then
+    curl -s "https://api.github.com/users/${1}/keys" | grep -E '^[[:space:]]*"key"' | cut -d '"' -f 4
+elif which wget >/dev/null 2>&1; then
+    curl -s "https://api.github.com/users/${1}/keys" | grep -E '^[[:space:]]*"key"' | cut -d '"' -f 4
 else
-    echo "This script requires JQ to be installed"
+    echo "curl and wget are not available"
+    exit 1
 fi
